d4f6797cba45374cccef0ea6b019942a81012b4a
Waseem Mansha <macuser@192.168.1.7>
2022-07-15 18:16:59 +0500

    server running hmtl

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require('path');
+
 const routes = require("./src/routes/index");
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1", routes);
-const port = process.env.PORT || 3200;
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const inventoryRoutes = require("./inventory");
-router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Apis running successfully",
-  });
-});
-
 router.use("/InventoryTransfer", inventoryRoutes);
 module.exports = router;
