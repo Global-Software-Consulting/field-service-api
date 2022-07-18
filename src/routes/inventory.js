f2faccfcd95dd6c64aa1707db341c6aca3359b0e
Ahtisham Shahzad <ahtishamshahzad@Ahtishams-MacBook-Pro.local>
2022-07-18 19:03:42 +0500

    add new  api route

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -12,4 +12,58 @@ const router = express.Router();
 router.get("/equipmentlist", (req, res) => {
   return res.status(200).json(readJsonFile("equipmentList.json"));
 });
+router.get("/equipmentlist/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("equipmentList.json"));
+});
+router.get("/truckMap/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("truckMap.json"));
+});
+
+router.get("/serviceorders/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("serviceorders.json"));
+});
+
+router.get("/crewNotes/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("crewNotes.json"));
+});
+router.get("/itemList", (req, res) => {
+  return res.status(200).json(readJsonFile("itemList.json"));
+});
+router.get("/topItemList", (req, res) => {
+  return res.status(200).json(readJsonFile("topItemList.json"));
+});
+router.get("/picklist/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("picklist.json"));
+});
+
+router.get("/unpostedMaterial/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("picklist.json"));
+});
+
+router.get("/unpostedMaterial/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("unpostedMaterial.json"));
+});
+
+router.get("/history/:id", (req, res) => {
+  return res.status(200).json(readJsonFile("history.json"));
+});
+
+router.post("/equipmentTransfer", (req, res) => {
+  setTimeout(() => {
+    return res.status(200).json({"documentData": null, "elapsed": "276", "errorCode": 0, "errorMessage": "Equipment Transfer successful.", "success": true});
+  }, 1000);
+});
+
+router.post("/projectInventoryTransfer", (req, res) => {
+  setTimeout(() => {
+    return res.status(200).json({"documentData": null, "elapsed": "276", "errorCode": 0, "errorMessage": "Equipment Transfer successful.", "success": true});
+  }, 1000);
+});
+router.post("/setDefaultCrewID", (req, res) => {
+setTimeout(() => {
+  return res.status(200).json({"documentData": null, "elapsed": "3", "errorCode": 0, "errorMessage": "", "success": true});
+}, 1000);
+});
+
+
 module.exports = router;
